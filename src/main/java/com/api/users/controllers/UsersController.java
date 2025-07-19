737f9ca3f125df3c42f7d2eefed982b4c33b532d
Emmanuel <eruacaballero@gmail.com>
2025-07-19 12:18:47 -0500

    Recuperacion de codigo

--- src/main/java/com/api/users/controllers/UsersController.java
+++ src/main/java/com/api/users/controllers/UsersController.java
@@ -1,5 +1,28 @@
 package com.api.users.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.users.dao.UsersDAO;
+import com.api.users.models.UsersModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/users")
 public class UsersController {
+    
+    @Autowired
+    private UsersDAO usersDAO;
+
+    // Metodo para obtener toda la lista de usuarios
+    @GetMapping("/getusers")
+    public ArrayList<UsersModel> getUsers() throws SQLException {
+        return usersDAO.getUsers();
+    }
     
 }
