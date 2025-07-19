bec6a4e31220117a457b87aa9ad7fd9567beb919
Emmanuel <eruacaballero@gmail.com>
2025-07-19 12:21:50 -0500

    Recuperacion de codigo

--- src/main/java/com/api/users/ApiUsersApplication.java
+++ src/main/java/com/api/users/ApiUsersApplication.java
@@ -1,13 +1,26 @@
 package com.api.users;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
+@RestController
 public class ApiUsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiUsersApplication.class, args);
+	}
+
+	// Redireccionamiento al controlador "UsersControllers"
+	@RequestMapping("/")
+	public void redirigir(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/users/getusers");
 	}
 
 }
