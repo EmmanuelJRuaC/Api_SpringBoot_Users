16f155e7231264d951a34bec1fc7eefa6bf5c342
Emmanuel <eruacaballero@gmail.com>
2025-07-18 20:15:55 -0500

    Configurando los parametros de la base de datos

--- src/main/java/com/api/users/factory/ConnectionFactory.java
+++ src/main/java/com/api/users/factory/ConnectionFactory.java
@@ -1,5 +1,20 @@
 package com.api.users.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    public Connection getConnection() {
+        String url = "jdbc:mysql://mysql-emmanuelrua.alwaysdata.net:3306/emmanuelrua_proyectotweb?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String user = "379340";
+        String password = "Ema20031011@";
+
+        try {
+            System.out.println("Conectado");
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
