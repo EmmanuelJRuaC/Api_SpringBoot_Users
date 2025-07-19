86c13f9c4e0f69db12f3070dbdf3b4d39f468463
Emmanuel <eruacaballero@gmail.com>
2025-07-18 21:13:57 -0500

    Correcion en la conexion de la base de datos

--- src/main/java/com/api/users/factory/ConnectionFactory.java
+++ src/main/java/com/api/users/factory/ConnectionFactory.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     public Connection getConnection() {
-        String url = "jdbc:mysql://mysql-emmanuelrua.alwaysdata.net:3306/emmanuelrua_proyectotweb?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-        String user = "379340";
-        String password = "Ema20031011@";
+        String url = "jdbc:mysql://mysql-emmanuelrua11.alwaysdata.net/emmanuelrua_users?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String user = "422146";
+        String password = "Ema20031011";
 
         try {
             System.out.println("Conectado");
