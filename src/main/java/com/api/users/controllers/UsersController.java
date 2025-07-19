378264691dbc6cd30d05312cddd1bd4e38005890
Emmanuel <eruacaballero@gmail.com>
2025-07-18 21:08:29 -0500

    Ruta /getusers terminada

--- src/main/java/com/api/users/controllers/UsersController.java
+++ src/main/java/com/api/users/controllers/UsersController.java
@@ -1,6 +1,14 @@
 package com.api.users.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.users.dao.UsersDAO;
+import com.api.users.models.UsersModel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UsersController {
     
+    @Autowired
+    private UsersDAO usersDAO;
+
     // Metodo para obtener toda la lista de usuarios
     @GetMapping("/getusers")
-    public String getUsers() {
-        return "COMENZAMOS CON EL PRIMER REDIRIGIMIENTO";
+    public ArrayList<UsersModel> getUsers() throws SQLException {
+        return usersDAO.getUsers();
     }
     
 }
