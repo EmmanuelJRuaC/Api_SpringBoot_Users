7fb66236356cad7ffafe9dcfb058a1350029342f
Emmanuel <eruacaballero@gmail.com>
2025-07-18 20:37:39 -0500

    Añadiendo datos a model: Users

--- src/main/java/com/api/users/models/UsersModel.java
+++ src/main/java/com/api/users/models/UsersModel.java
@@ -1,5 +1,22 @@
 package com.api.users.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class UsersModel {
     
+    @Id
+    private Long id;
+
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private String address;
+    private String city;
+    private int phone;
 }
