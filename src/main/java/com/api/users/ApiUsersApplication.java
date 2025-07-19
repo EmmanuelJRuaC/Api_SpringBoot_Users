5e8bdcd6c56f8c174e6968cf30b89ec762ee20d4
Emmanuel <eruacaballero@gmail.com>
2025-07-19 12:35:28 -0500

    Correciones

--- src/main/java/com/api/users/ApiUsersApplication.java
+++ src/main/java/com/api/users/ApiUsersApplication.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/users")
 public class ApiUsersApplication {
 
 	public static void main(String[] args) {
