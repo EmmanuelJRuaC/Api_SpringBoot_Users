ca8eaaf0a58eafadc1a71f02a813240290b441ae
Emmanuel <eruacaballero@gmail.com>
2025-07-19 13:35:21 -0500

    Correcione metodo getuserbyid

--- src/main/java/com/api/users/controllers/UsersController.java
+++ src/main/java/com/api/users/controllers/UsersController.java
@@ -7,8 +7,10 @@ import com.api.users.models.UsersModel;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-    
+
     @Autowired
     private UsersDAO usersDAO;
 
@@ -25,9 +27,22 @@ public class UsersController {
     public ArrayList<UsersModel> getUsers() throws SQLException {
         return usersDAO.getUsers();
     }
-    
+
+    // Metodo busqueda de usuario por ID
     @GetMapping("/getuser/{id}")
-    public ArrayList<UsersModel> getUserById(@PathVariable long id) throws SQLException {
-        return usersDAO.getUserById(id);
+    public ResponseEntity<?> getUserById(@PathVariable long id) throws SQLException {
+        ArrayList<UsersModel> user = new ArrayList<>();
+        user.addAll(usersDAO.getUserById(id));
+
+        if (user.isEmpty()) {
+            // Retornar un JSON con un mensaje personalizado
+            return ResponseEntity
+                    .status(404)
+                    .body(Map.of("error", "Usuario no encontrado"));
+        } else {
+            return ResponseEntity.ok(user); // retorna los datos encontrados
+        }
     }
+
+
 }
