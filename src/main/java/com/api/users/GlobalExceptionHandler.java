d8efec6e42b634bdf207a43fcc65dcaecfc7d5a1
Emmanuel <eruacaballero@gmail.com>
2025-07-22 19:08:57 -0500

    Nuevo archivo de expeciones

---
+++ src/main/java/com/api/users/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.api.users;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidJson(String msg) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(Map.of("Error", msg));
+    }
+}
+
